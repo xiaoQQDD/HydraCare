d82070a9764a782b780e6971da2dc7d41afc47f3
xiaoQQDD <ddyy6537fjv@163.com>
2023-12-07 01:44:13 +0800

    merge  remind

--- functiondemo/__init__.py
+++ functiondemo/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:server/__init__.py
 """app module"""
 import os
 import datetime as dt
@@ -58,64 +57,3 @@ def create_app(test_config=None):
     # app.add_url_rule('/', endpoint='index')
 
     return app
-=======
-"""app module"""
-import os
-import datetime as dt
-from flask import Flask, render_template
-
-from . import auth
-from . import rest
-
-
-
-
-
-def create_app(test_config=None):
-    """create and configure the app"""
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY=b'_lkdiokHlkjl*eoiZKLGUIkreiuh' +
-        str(dt.datetime.now()).encode('utf-8'),
-        DATABASE=os.path.join(app.instance_path, 'sqlite3.db'),
-        SQLALCHEMY_DATABASE_URI="sqlite:///" +
-        os.path.join(app.instance_path, 'sqlite3.db'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # dbal.init_app(app)
-
-    @app.errorhandler(404)
-    def page_not_found(e):
-        return render_template('404.html'), 404
-
-    @app.errorhandler(500)
-    def server_error(e):
-        return render_template('500.html'), 500
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    app.debug = True
-
-    app.register_blueprint(auth.bp)
-
-    app.register_blueprint(rest.bp)
-    # app.add_url_rule('/', endpoint='index')
-
-    return app
->>>>>>> ae3b4160909a27ffe7fd44aebd9f3a46d11d1e9b:functiondemo/__init__.py
